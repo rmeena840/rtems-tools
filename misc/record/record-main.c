5a1c7b6e3c0c5246a8afc538f9f01576a5ac7983
Ravindra <rmeena840@gmail.com>
2019-07-11 10:04:17 +0530

    CTF header added

--- misc/record/record-main.c
+++ misc/record/record-main.c
@@ -70,9 +70,10 @@ typedef struct client_item {
   uint64_t                     counter;
 } client_item;
 
-typedef struct ctf_stream {
+typedef struct ctf_header {
   uint32_t                     stream_id;
-} ctf_stream;
+  uint32_t                     cpu_id;
+} ctf_header;
 
 typedef struct ctf_event {
   uint64_t                     ns;
@@ -277,7 +278,7 @@ int main( int argc, char **argv )
   rtems_record_client_context ctx;
   client_context cctx;
   client_item *items;
-  ctf_stream ctf_stream;
+  ctf_header ctf_header;
   const char *host;
   uint16_t port;
   const char *input_file;
@@ -348,11 +349,12 @@ int main( int argc, char **argv )
 
     event_streams[ i ] = fopen( filename , "wb" );
 
-    ctf_stream.stream_id = (uint32_t) i;
+    ctf_header.stream_id = (uint32_t) 0;
+    ctf_header.cpu_id = (uint32_t) i;
 
-    // stream_id of each file. It is needed to be added the very begining of
+    // stream_id = 0 and cpu_id of each file. It is needed to be added the very begining of
     // each stream file
-    fwrite( &ctf_stream, sizeof( ctf_stream ), 1, event_streams[ i ] );
+    fwrite( &ctf_header, sizeof( ctf_header ), 1, event_streams[ i ] );
 
     assert( event_streams[ i ] != NULL );
     cctx.event_streams[ i ] = event_streams[ i ];
